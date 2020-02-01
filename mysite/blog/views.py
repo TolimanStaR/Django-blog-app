9726b3a1db989d9a6c9b75c52d908dfd24ad07b3
TolimanStaR <toliman.st4r@gmail.com>
2020-02-01 16:17:58 +0300

    add comments model and form

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 
 # def post_list(request):
@@ -18,6 +20,31 @@ from django.views.generic import ListView
 #         posts = paginator.page(paginator.num_pages)
 #
 #     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method:
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "' \
+                      '{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: ' \
+                      '{}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog', [cd['to']])
+            sent = True
+
+            return render(request, 'blog/post/share.html',
+                          {'post': post, 'form': form, 'sent': sent})
+
+        else:
+            form = EmailPostForm()
+            return render(request, 'blog/post/share.html',
+                          {'post': post, 'form': form, 'sent': sent})
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
